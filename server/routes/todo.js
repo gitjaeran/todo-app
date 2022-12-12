0e07bc8cc0119e0811971d5483dbf317a3a43dec
gitjaeran <devjaeran@gmail.com>
2022-12-12 12:25:24 +0900

    feat: Todo 수정 및 삭제 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -30,4 +30,55 @@ router.post('/todo', async (req, res) => {
     res.send(err);
   }
 });
+
+//PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+router.patch('/todo/:todoId', async (req, res) => {
+try {
+let [isUpdated] = await Todo.update(
+  {
+    title: req.body.title,
+    done: req.body.done,
+},
+{
+    where: {
+      id: req.params.todoId,
+  },
+}
+)
+console.log(isUpdated)//수정성공시[1] -> 1, 수정실패시[0] -> 0
+
+// !0
+if (!isUpdated) {
+  return res.send(false)
+}
+res.send(true)
+} catch (err){
+  res.send(err);
+}
+});
+
+//DELETE localhost:PORT/todo/:todoId - remove a specific todo (DELETE)
+router.delete('/todo/:todoId', async (req, res) => {
+  // console.log(req.params); // { todoId: '8' }
+
+  try {
+    let isDeleted = await Todo.destroy({
+      where: {
+        id: req.params.todoId,
+      },
+    });
+    // console.log(isDeleted); // 1 or 0
+
+    // 삭제 실패 (!0)
+    if (!isDeleted) {
+      return res.send(false);
+    }
+
+    // 삭제 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
