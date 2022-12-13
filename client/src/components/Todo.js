d0ce897a75b4bf898b57c1c297f28c275924e52d
gitjaeran <devjaeran@gmail.com>
2022-12-13 15:00:11 +0900

    feat: Todo 수정 기능 구현

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -5,9 +5,40 @@ const {id, title, done} = item;
 
 const [todoItem, setTodoItem] = useState(item)
 
+const [readOnly, setReadOnly] = useState(true)
+
 const onDeleteBtnClick = () => {
   deleteItem(todoItem);
 }
+
+//title input 커서가 깜빡인다고 수정 가능한것은 아님
+//사용자가 키 입력할 때마다 todoItem의 title을 새 값으로 변경
+const editEventHandler = (e) => {
+  //rest: id, done 정보
+  const { title, ...rest } = todoItem;
+
+  setTodoItem({
+    title: e.target.value,
+    ...rest
+  })
+}
+
+//title input 클릭시 readOnly state를 false로 변경
+const offReadOnlyMode = () => {
+  setReadOnly(false);
+}
+
+//title input에서 enter key 입력시 title 수정 완료 = readOnly state를 true로 변경
+const enterKeyEventHandler = (e) => {
+  if (e.key === 'Enter') {
+    setReadOnly(true)
+  }
+}
+
+// const checkBoxEventHandler = () => {
+//   todoItem.done = !todoItem.done //true <-> false
+//   setTodoItem(todoItem)
+// }
 
   return (
     <div className="Todo">
@@ -17,11 +48,19 @@ const onDeleteBtnClick = () => {
         name={`todo${id}`}  
         value={`todo${id}`} 
         defaultChecked={done}
+        // onChange={checkBoxEventHandler}
       />
         
-      <label 
-        htmlFor={`todo${id}`}>{title}
-      </label>
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input 
+      type="text"
+      id={`todo${id}`}
+      value={todoItem.title}
+      onChange={editEventHandler}
+      onKeyPress={enterKeyEventHandler}
+      onClick={offReadOnlyMode}
+      readOnly={readOnly}/>
+
       <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   )
