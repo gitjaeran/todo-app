4a8687dcb4bd2dd07835ba887b781f40739b953c
gitjaeran <devjaeran@gmail.com>
2022-12-14 09:40:57 +0900

    fix: Todo 수정 오류 해결

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,69 +1,73 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-const Todo = ({item, deleteItem}) => {
-const {id, title, done} = item;
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
+  const { id, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+  const [readOnly, setReadOnly] = useState(true);
 
-const [todoItem, setTodoItem] = useState(item)
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
 
-const [readOnly, setReadOnly] = useState(true)
+  // title input 커서가 깜빡인다고 수정이 가능한 것은 아님
+  // 사용자가 키보드 입력할 때마다 todoItem의 title을 새 값으로 변경
+  const editEventHandler = (e) => {
+    // rest: id, done 정보
+    const { title, ...rest } = todoItem; // { id: 1, title: 'todo1', done: false, }
 
-const onDeleteBtnClick = () => {
-  deleteItem(todoItem);
-}
+    setTodoItem({
+      title: e.target.value,
+      ...rest,
+    });
+  };
 
-//title input 커서가 깜빡인다고 수정 가능한것은 아님
-//사용자가 키 입력할 때마다 todoItem의 title을 새 값으로 변경
-const editEventHandler = (e) => {
-  //rest: id, done 정보
-  const { title, ...rest } = todoItem;
+  // title input 클릭시 (title를 수정하겠다!!) : readOnly state를 false로 변경
+  const offReadOnlyMode = () => {
+    setReadOnly(false);
+  };
 
-  setTodoItem({
-    title: e.target.value,
-    ...rest
-  })
-}
+  // title input에서 enter 키 입력시 (title 수정을 완료했다!!): readOnly state를 true로 변경
+  const enterKeyEventHandler = (e) => {
+    if (e.key === 'Enter') {
+      setReadOnly(true);
+    }
+  };
 
-//title input 클릭시 readOnly state를 false로 변경
-const offReadOnlyMode = () => {
-  setReadOnly(false);
-}
-
-//title input에서 enter key 입력시 title 수정 완료 = readOnly state를 true로 변경
-const enterKeyEventHandler = (e) => {
-  if (e.key === 'Enter') {
-    setReadOnly(true)
-  }
-}
-
-// const checkBoxEventHandler = () => {
-//   todoItem.done = !todoItem.done //true <-> false
-//   setTodoItem(todoItem)
-// }
+  // checkbox 업데이트
+  // done: true -> false, fasle, -> true
+  const checkboxEventHandler = (e) => {
+    // rest: id, title 정보
+    const { done, ...rest } = todoItem; // { id: 1, title: 'todo1', done: false, }
+    setTodoItem({
+      done: e.target.checked,
+      ...rest,
+    });
+  };
 
   return (
     <div className="Todo">
-      <input 
-        type="checkbox" 
-        id={`todo${id}`} 
-        name={`todo${id}`}  
-        value={`todo${id}`} 
+      <input
+        type="checkbox"
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
         defaultChecked={done}
-        // onChange={checkBoxEventHandler}
+        onChange={checkboxEventHandler}
       />
-        
       {/* <label htmlFor={`todo${id}`}>{title}</label> */}
-      <input 
-      type="text"
-      id={`todo${id}`}
-      value={todoItem.title}
-      onChange={editEventHandler}
-      onKeyPress={enterKeyEventHandler}
-      onClick={offReadOnlyMode}
-      readOnly={readOnly}/>
-
+      <input
+        type="text"
+        value={todoItem.title}
+        onChange={editEventHandler}
+        onClick={offReadOnlyMode}
+        onKeyPress={enterKeyEventHandler}
+        readOnly={readOnly}
+      />
       <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
+
