7c76cd1854c6d818d85a62bb4cfac60ca7ed7208
gitjaeran <devjaeran@gmail.com>
2022-12-15 12:04:41 +0900

    feat: UPDATE API 연결

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import { faPlus, faTrashAlt, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import '../styles/Todo.scss'
+import '../styles/Todo.scss';
 
-const Todo = ({ item, deleteItem }) => {
+const Todo = ({ item, deleteItem, updateItem }) => {
   // console.log(item); // { id: 1, title: 'todo1', done: false, }
-  const { id, done } = item;
+  const { id, title, done } = item;
   const [todoItem, setTodoItem] = useState(item);
   const [readOnly, setReadOnly] = useState(true);
 
@@ -34,6 +34,7 @@ const Todo = ({ item, deleteItem }) => {
   const enterKeyEventHandler = (e) => {
     if (e.key === 'Enter') {
       setReadOnly(true);
+      updateItem(todoItem); // 수정1 (엔터 누르면 저장)
     }
   };
 
@@ -42,10 +43,12 @@ const Todo = ({ item, deleteItem }) => {
   const checkboxEventHandler = (e) => {
     // rest: id, title 정보
     const { done, ...rest } = todoItem; // { id: 1, title: 'todo1', done: false, }
-    setTodoItem({
+    const updatedItem = {
       done: e.target.checked,
       ...rest,
-    });
+    };
+    setTodoItem(updatedItem);
+    updateItem(updatedItem); // 수정2(체크박스 변경시 저장) -> 변경될 대상
   };
 
   return (
@@ -67,10 +70,11 @@ const Todo = ({ item, deleteItem }) => {
         onKeyPress={enterKeyEventHandler}
         readOnly={readOnly}
       />
-      <button onClick={onDeleteBtnClick}><FontAwesomeIcon icon={faTrashCan} /></button>
+      <button onClick={onDeleteBtnClick}>
+        <FontAwesomeIcon icon={faTrashCan} />
+      </button>
     </div>
   );
 };
 
 export default Todo;
-
